014cec4271eff0cdea41c3033447db48f8857806
EshwarNaikG <eshwarnaik79@gmail.com>
2025-12-24 16:34:07 +0530

    contact form updated and message receives in email

--- script2.js
+++ script2.js
@@ -9,20 +9,20 @@ menuBtn.addEventListener("click", () => {
 
 // CONTACT FORM (Frontend Simulation)
 
-// document.getElementById("contactForm").addEventListener("submit", function (e) {
-//     e.preventDefault();
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-//     let name = document.getElementById("name").value;
-//     let email = document.getElementById("email").value;
-//     let message = document.getElementById("message").value;
-//     let status = document.getElementById("formStatus");
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let status = document.getElementById("formStatus");
 
-//     if (name && email && message) {
-//         status.style.color = "lightgreen";
-//         status.innerText = "Message sent successfully!";
-//         this.reset();
-//     } else {
-//         status.style.color = "red";
-//         status.innerText = "Please fill all fields!";
-//     }
-// });
+    if (name && email && message) {
+        status.style.color = "lightgreen";
+        status.innerText = "Message sent successfully!";
+        this.reset();
+    } else {
+        status.style.color = "red";
+        status.innerText = "Please fill all fields!";
+    }
+});
